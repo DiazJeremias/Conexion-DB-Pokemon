49f05f9bcf42e7b2c564c409c9129aac98456535
Jeremias <diazjeremias95@gmail.com>
2024-05-23 16:04:09 -0300

    Debilidades cargadas

--- ejemplos-ado-net/winform-app/Form1.Designer.cs
+++ ejemplos-ado-net/winform-app/Form1.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvPokemons = new System.Windows.Forms.DataGridView();
+            this.pbxPokemon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPokemon)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvPokemons
+            // 
+            this.dgvPokemons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemons.Location = new System.Drawing.Point(31, 33);
+            this.dgvPokemons.Name = "dgvPokemons";
+            this.dgvPokemons.Size = new System.Drawing.Size(584, 258);
+            this.dgvPokemons.TabIndex = 0;
+            this.dgvPokemons.SelectionChanged += new System.EventHandler(this.dgvPokemons_SelectionChanged);
+            // 
+            // pbxPokemon
+            // 
+            this.pbxPokemon.Location = new System.Drawing.Point(636, 33);
+            this.pbxPokemon.Name = "pbxPokemon";
+            this.pbxPokemon.Size = new System.Drawing.Size(286, 258);
+            this.pbxPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPokemon.TabIndex = 1;
+            this.pbxPokemon.TabStop = false;
             // 
             // frmPokemon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(965, 391);
+            this.Controls.Add(this.pbxPokemon);
+            this.Controls.Add(this.dgvPokemons);
             this.Name = "frmPokemon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPokemon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPokemon)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemons;
+        private System.Windows.Forms.PictureBox pbxPokemon;
     }
 }
 
--- ejemplos-ado-net/winform-app/Form1.cs
+++ ejemplos-ado-net/winform-app/Form1.cs
@@ -12,9 +12,40 @@ namespace winform_app
 {
     public partial class frmPokemon : Form
     {
+        private List<Pokemon> listaPokemon;
         public frmPokemon()
         {
             InitializeComponent();
         }
+
+        private void frmPokemon_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            listaPokemon = negocio.listar();
+            dgvPokemons.DataSource = listaPokemon;
+            dgvPokemons.Columns["UrlImagen"].Visible = false;
+            cargarImagen(listaPokemon[0].UrlImagen);
+        }
+
+        private void dgvPokemons_SelectionChanged(object sender, EventArgs e)
+        {
+            Pokemon seleccionado = (Pokemon)dgvPokemons.CurrentRow.DataBoundItem;
+            cargarImagen(seleccionado.UrlImagen);
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbxPokemon.Load(imagen);
+
+            }
+            catch (Exception)
+            {
+
+                pbxPokemon.Load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkHbLOf8uW56x79abBDHXJTqcfgNcx0wAe4XGfxn8-CA&s");
+            }
+        }
+
     }
 }
